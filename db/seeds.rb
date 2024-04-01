afda96b613c3b4868f5abcbc2b7d09412fb9b211
akira onishi <akira.frlnc@gmail.com>
2024-04-01 15:43:07 +0200

    migrated and seeded with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+require 'faker'
+
+10.times do
+  Product.create(
+    name: Faker::Commerce.product_name
+  )
+end
+
+# db/seeds.rb
+require 'faker'
+
+Product.all.each do |product|
+  5.times do
+    Review.create(
+      rating: Faker::Number.between(from: 1, to: 5),
+      comment: Faker::Lorem.paragraph(sentence_count: 2),
+      product: product
+    )
+  end
+end
